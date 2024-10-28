fa2776154d475f60a14a8128b3b30a40df5c45a0
BlackLazuli <broxephos@gmail.com>
2024-10-28 23:09:46 +0800

    Updated QuizEntity

--- swift/src/main/java/com/g2appdev/swift/entity/QuizEntity.java
+++ swift/src/main/java/com/g2appdev/swift/entity/QuizEntity.java
@@ -1,56 +1,65 @@
 package com.g2appdev.swift.entity;
 
-import jakarta.persistence.*;
-import java.util.List;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+//import jakarta.persistence.OneToMany;
+//import jakarta.persistence.CascadeType;
+//import jakarta.persistence.FetchType;
+//import java.util.List;
 
 @Entity
 public class QuizEntity {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int quiz_id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int quiz_id;
+	
+	private String title;
+	private String questions;
+	private int score;
+	
+	public QuizEntity() {
+		super();
+	}
+	
+	public QuizEntity(int quiz_id, String title, String questions, int score) {
+		super();
+		this.quiz_id = quiz_id;
+		this.title = title;
+		this.questions = questions;
+		this.score = score;
+	}
 
-    private String title;
-    private List<String> questions;
-    private int score;
-    
-    public QuizEntity() {
-        super();
-    }
-
-    public QuizEntity(int quiz_id, String title, List<String> questions, int score) {
-        super();
-        this.quiz_id = quiz_id;
-        this.title = title;
-        this.questions = questions;
-        this.score = score;
-    }
-
-    public int getQuizId() {
+    public int getQuizId(){
         return quiz_id;
     }
 
-    public String getTitle() {
-        return title;
+    public void setQuizId(int quiz_id){
+        this.quiz_id = quiz_id;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
+	public String getTitle() {
+		return title;
+	}
 
-    public List<String> getQuestions() {
-        return questions; // Update the getter
-    }
+	public void setTitle(String title) {
+		this.title = title;
+	}
 
-    public void setQuestions(List<String> questions) { // Update the setter
-        this.questions = questions;
-    }
+	public String getQuestions() {
+		return questions;
+	}
 
-    public int getScore() {
-        return score;
-    }
+	public void setQuestions(String questions) {
+		this.questions = questions;
+	}
 
-    public void setScore(int score) {
-        this.score = score;
-    }
+	public int getScore() {
+		return score;
+	}
 
+	public void setScore(int score) {
+		this.score = score;
+	}
 }
