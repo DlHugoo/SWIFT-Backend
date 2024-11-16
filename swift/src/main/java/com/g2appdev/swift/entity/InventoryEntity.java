7f9b4d6fb475b14fd2d4e203ee662d774f845f85
KDazai <feerdeellaban556@gmail.com>
2024-11-16 22:13:21 +0800

    Connecting ShopEntity and InventoryEntity

--- swift/src/main/java/com/g2appdev/swift/entity/InventoryEntity.java
+++ swift/src/main/java/com/g2appdev/swift/entity/InventoryEntity.java
@@ -1,55 +1,65 @@
 package com.g2appdev.swift.entity;
 
+import java.util.List;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "tblinventory")
 public class InventoryEntity {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int inventoryId;
-	
+
 	private String itemList;
 	private int totalCoins;
-	
-	/*@ManyToMany
-	@JoinTable(
-	        name = "inventory_shop_items",
-	        joinColumns = @JoinColumn(name = "inventory_id"),
-	        inverseJoinColumns = @JoinColumn(name = "shop_id")
-	    )
-	 private Set<ShopEntity> items;*/
+
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "itemId", cascade = CascadeType.ALL)
+	private List<ShopEntity> items;
+
 	public InventoryEntity() {
-		
+
 	}
+
 	public InventoryEntity(int inventoryId, String itemList, int totalCoins) {
 		super();
 		this.inventoryId = inventoryId;
-		this.itemList=itemList;
+		this.itemList = itemList;
 		this.totalCoins = totalCoins;
-		
+
 	}
+
 	public int getInventoryId() {
 		return inventoryId;
 	}
+
 	public String getItemList() {
 		return itemList;
 	}
+
 	public void setItemList(String itemList) {
 		this.itemList = itemList;
 	}
+
 	public int getTotalCoins() {
 		return totalCoins;
 	}
+
 	public void setTotalCoins(int totalCoins) {
 		this.totalCoins = totalCoins;
 	}
-	/*public Set<ShopEntity> getItems() {
+
+	public List<ShopEntity> getItems() {
 		return items;
 	}
-	public void setItems(Set<ShopEntity> items) {
+
+	public void setItems(List<ShopEntity> items) {
 		this.items = items;
-	}*/
+	}
 }
